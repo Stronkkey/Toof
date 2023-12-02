84cc3daab332e8a3805fe9ba17407173ff0c83e4
Stronkkey <Ail45000@proton.me>
2023-12-02 14:50:28 +0100

    Add texture_region variable for SpriteItem

--- items/sprite.cpp
+++ items/sprite.cpp
@@ -2,15 +2,33 @@
 
 using namespace sdl;
 
+void SpriteItem::update_texture() {
+  RenderingServer *rendering_server = get_rendering_server();
+  if (!rendering_server)
+    return;
+
+  rendering_server->canvas_item_clear(canvas_item);
+
+  if (texture_region == Rect2i::EMPTY)
+    rendering_server->canvas_item_add_texture_region(texture, canvas_item, rendering_server->texture_get_source_region(texture));
+  else
+   rendering_server->canvas_item_add_texture_region(texture, canvas_item, texture_region);
+}
+
 void SpriteItem::set_texture(const uid &new_texture) {
   texture = new_texture;
-  RenderingServer *rendering_server = get_rendering_server();
-  if (rendering_server) {
-    rendering_server->canvas_item_clear(canvas_item);
-    rendering_server->canvas_item_add_texture(new_texture, canvas_item);
-  }
+  update_texture();
 }
 
 uid SpriteItem::get_texture() const {
   return texture;
+}
+
+void SpriteItem::set_texture_region(const Rect2i &new_texture_region) {
+  texture_region = new_texture_region;
+  update_texture();
+}
+
+Rect2i SpriteItem::get_texture_region() const {
+  return texture_region;
 }
--- items/sprite.hpp
+++ items/sprite.hpp
@@ -6,12 +6,19 @@
 namespace sdl {
 
 class SpriteItem : public RenderingItem {
+
 private:
   uid texture;
+  Rect2i texture_region;
+
+  void update_texture();
 
 public:
   void set_texture(const uid &new_texture);
   uid get_texture() const;
+
+  void set_texture_region(const Rect2i &new_texture_region);
+  Rect2i get_texture_region() const;
 
 };
 
