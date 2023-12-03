be197546802bd5586b46085a323e2f8e2ba6b050
Stronkkey <Ail45000@proton.me>
2023-12-03 15:51:02 +0100

    Add build option for for real_t and int_t

--- types/math_defs.hpp
+++ types/math_defs.hpp
@@ -7,16 +7,20 @@ namespace sdl {
 
 #ifdef REAL_T_IS_DOUBLE
 typedef double real_t;
+#pragma message("DOUBLE")
 #else
 typedef float real_t;
+#pragma message("FLOAT")
 #endif
 
 #ifdef INT_IS_64BIT
 typedef int64_t int_t;
 typedef uint64_t uint_t;
+#pragma message("64BIT")
 #else
 typedef int32_t int_t;
 typedef uint32_t uint_t;
+#pragma message("32BIT")
 #endif
 
 }
