0b16b99d0fa755c62226691220cd0a67507b19e4
Stronkkey <Ail45000@proton.me>
2023-12-30 20:33:34 +0100

    Remove error macros

--- types/error_macros.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace sdl {
-
-#define ERROR_FAIL_CONDITION(condition) if (condition) return;
-#define ERROR_FAIL_CONDITION_RETURN(condition, return_value) if (condition) return return_value;
-
-}
