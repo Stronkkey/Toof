478627677c3fa9673eff6e3427459c030b884940
Stronkkey <Ail45000@proton.me>
2024-01-03 19:07:58 +0100

    Add is_equal_approx to UtilityFunctions

--- types/utility_functions.cpp
+++ types/utility_functions.cpp
@@ -1,5 +1,9 @@
-#include <cstdarg>
 #include <types/utility_functions.hpp>
+#include <types/vector2.hpp>
+#include <types/rect2.hpp>
+#include <types/transform2d.hpp>
+
+#include <SDL_timer.h>
 
 #include <iostream>
 
@@ -17,4 +21,20 @@ void UtilityFunctions::print(const std::string &string) {
 
 void UtilityFunctions::prints(const std::string &string) {
 	std::cout << string + "\n";
+}
+
+bool UtilityFunctions::is_equal_approx(const real_t left, const real_t right) {
+	return left >= (right - EPSILON) && left <= (right + EPSILON);
+}
+
+bool UtilityFunctions::is_equal_approx(const Vector2 &left, const Vector2 &right) {
+	return is_equal_approx(left.x, right.x) && is_equal_approx(left.y, right.y);
+}
+
+bool UtilityFunctions::is_equal_approx(const Rect2 &left, const Rect2 &right) {
+	return is_equal_approx(left.get_position(), right.get_position()) && is_equal_approx(left.get_size(), right.get_size());
+}
+
+bool UtilityFunctions::is_equal_approx(const Transform2D &left, const Transform2D &right) {
+	return is_equal_approx(left.origin, right.origin) && is_equal_approx(left.rotation, right.rotation) && is_equal_approx(left.scale, right.scale);
 }
--- types/utility_functions.hpp
+++ types/utility_functions.hpp
@@ -3,17 +3,26 @@
 #include <types/math_defs.hpp>
 
 #include <string>
-#include <vector>
-#include <SDL_timer.h>
 
 namespace sdl {
 
+struct Vector2;
+struct Rect2;
+struct Transform2D;
+
 namespace UtilityFunctions {
+
+const real_t EPSILON = 0.0000000001;
 
 real_t get_passed_time();
 
 void print(const std::string &string);
 void prints(const std::string &string);
+
+bool is_equal_approx(const real_t left, const real_t right);
+bool is_equal_approx(const Vector2 &left, const Vector2 &right);
+bool is_equal_approx(const Rect2 &left, const Rect2 &right);
+bool is_equal_approx(const Transform2D &left, const Transform2D &right);
 
 }
 
