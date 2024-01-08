ab347d4f416af2c7e1988d48c994d4904ad9615e
Stronkkey <Ail45000@proton.me>
2024-01-08 19:08:28 +0100

    Temp

---
+++ servers/physics_server.cpp
@@ -0,0 +1,8 @@
+#include <servers/physics/physics_body.hpp>
+#include <servers/physics/physics_shape.hpp>
+#include <servers/physics_server.hpp>
+
+void sdl::PhysicsServer2D::tick(const double delta) {
+	for (const std::pair<uid, std::shared_ptr<PhysicsBody>> iterator: bodies) {e
+	}
+}
--- servers/physics_server.hpp
+++ servers/physics_server.hpp
@@ -3,15 +3,22 @@
 #include <core/math/math_defs.hpp>
 
 #include <unordered_map>
+#include <memory>
 
 namespace sdl {
 
 struct PhysicsBody;
+struct PhysicsShape;
 
 class PhysicsServer2D {
 
 private:
-	std::unordered_map<uid, PhysicsBody> bodies;
+	std::unordered_map<uid, std::shared_ptr<PhysicsBody>> bodies;
+	std::unordered_map<uid, std::shared_ptr<PhysicsShape>> shapes;
+
+public:
+	void tick(const double delta);
+
 };
 
 }
