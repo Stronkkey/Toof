e22d72b9caee7673d132aec18b6d55bdf55fe569
Stronkkey <Ail45000@proton.me>
2024-02-19 17:23:59 +0100

    Make transform2d constexpr and use angle class instead of double

--- core/math/transform2d.cpp
+++ core/math/transform2d.cpp
@@ -6,56 +6,15 @@ using namespace sdl;
 
 const Transform2D Transform2D::IDENTITY = Transform2D(0, 0, 0, 1, 1);
 
-Transform2D::Transform2D(): rotation(0.0), origin(Vector2::ZERO), scale(Vector2::ONE) {
-}
-
-Transform2D::Transform2D(const double new_rotation, const Vector2 &new_origin, const Vector2 &new_scale): rotation(new_rotation), origin(new_origin), scale(new_scale) {
-}
-
-Transform2D::Transform2D(const double new_rotation, const real_t origin_x, const real_t origin_y, const real_t scale_x, const real_t scale_y):
-    rotation(new_rotation),
-    origin(Vector2(origin_x, origin_y)),
-    scale(Vector2(scale_x, scale_y)) {
-}
-
-Transform2D::Transform2D(const Transform2D &transform2d): rotation(transform2d.rotation), origin(transform2d.origin), scale(transform2d.scale) {
-}
-
 #ifdef B2_INCLUDED
 Transform2D::Transform2D(const b2Transform &b2_transform): rotation(Math::radians_to_degrees(b2_transform.q.GetAngle())), origin(b2_transform.p), scale(Vector2::ONE) {
 }
-#endif
 
-bool Transform2D::operator==(const Transform2D &right) const {
-	return origin == right.origin && rotation == right.rotation;
-}
-
-bool Transform2D::operator!() const {
-	return origin == Vector2::ZERO || rotation == 0.0;
-}
-
-Transform2D Transform2D::operator*(const Transform2D &right) const {
-	return Transform2D(rotation + right.rotation, origin + right.origin, scale * right.scale);
-}
-
-#ifdef B2_INCLUDED
 b2Transform Transform2D::to_b2_transform() const {
-	return b2Transform(origin.to_b2_vec2(), b2Rot(Math::degrees_to_radians(rotation)));
+	return b2Transform(origin.to_b2_vec2(), b2Rot(rotation.get_angle_radians()));
 }
 #endif
 
 std::string Transform2D::to_string() const {
-	return "[Scale: " + scale.to_string() + ", Origin: " + origin.to_string() + ", Rotation: " + std::to_string(rotation) + " degrees" + "]";
-}
-
-void Transform2D::operator=(const Transform2D &right) {
-	origin = right.origin;
-	rotation = right.rotation;
-	scale = right.scale;
-}
-
-void Transform2D::operator*=(const Transform2D &right) {
-	origin += right.origin;
-	rotation += right.rotation;
-	scale *= right.scale;
+	return "[Scale: " + scale.to_string() + ", Origin: " + origin.to_string() + ", Rotation: " + std::to_string(rotation.get_angle_degrees()) + " degrees" + "]";
 }
--- core/math/transform2d.hpp
+++ core/math/transform2d.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <core/math/vector2.hpp>
+#include <core/math/angle.hpp>
 
 #ifdef B2_INCLUDED
 struct b2Transform;
@@ -9,32 +10,72 @@ struct b2Transform;
 namespace sdl {
 
 struct Transform2D {
-	Transform2D();
-	Transform2D(const double new_rotation, const Vector2 &new_origin, const Vector2 &new_scale);
-	Transform2D(const double new_rotation, const real_t origin_x, const real_t origin_y, const real_t scale_x, const real_t scale_y);
-	Transform2D(const Transform2D &transform2d);
+	constexpr Transform2D();
+	constexpr Transform2D(const Angle rotation, const Vector2 &new_origin, const Vector2 &new_scale);
+	constexpr Transform2D(const Angle rotation, const real_t origin_x, const real_t origin_y, const real_t scale_x, const real_t scale_y);
+	constexpr Transform2D(const Transform2D &transform2d);
 	#ifdef B2_INCLUDED
 	Transform2D(const b2Transform &b2_transform);
 	#endif
 
-	double rotation;
+	Angle rotation;
 	Vector2 origin;
 	Vector2 scale;
 
-	bool operator==(const Transform2D &right) const;
-	bool operator!() const;
+	constexpr bool operator==(const Transform2D &right) const;
+	constexpr bool operator!() const;
 
-	Transform2D operator*(const Transform2D &right) const;
+	constexpr Transform2D operator*(const Transform2D &right) const;
 
 	#ifdef B2_INCLUDED
 	[[nodiscard]] b2Transform to_b2_transform() const;
 	#endif
 	[[nodiscard]] std::string to_string() const;
 
-	void operator=(const Transform2D &right);
-	void operator*=(const Transform2D &right);
+	constexpr void operator=(const Transform2D &right);
+	constexpr void operator*=(const Transform2D &right);
 
 	static const Transform2D IDENTITY;
 };
+
+constexpr Transform2D::Transform2D(): rotation(0.0), origin(Vector2::ZERO), scale(Vector2::ONE) {
+}
+
+constexpr Transform2D::Transform2D(const Angle rotation, const Vector2 &new_origin, const Vector2 &new_scale): rotation(rotation), origin(new_origin), scale(new_scale) {
+}
+
+constexpr Transform2D::Transform2D(const Angle rotation, const real_t origin_x, const real_t origin_y, const real_t scale_x, const real_t scale_y):
+    rotation(rotation),
+    origin(Vector2(origin_x, origin_y)),
+    scale(Vector2(scale_x, scale_y)) {
+}
+
+constexpr Transform2D::Transform2D(const Transform2D &transform2d): rotation(transform2d.rotation), origin(transform2d.origin), scale(transform2d.scale) {
+}
+
+constexpr bool Transform2D::operator==(const Transform2D &right) const {
+	return origin == right.origin && rotation == right.rotation;
+}
+
+constexpr bool Transform2D::operator!() const {
+	return origin == Vector2::ZERO || rotation == 0.0;
+}
+
+constexpr Transform2D Transform2D::operator*(const Transform2D &right) const {
+	return Transform2D(rotation + right.rotation, origin + right.origin, scale * right.scale);
+}
+
+constexpr void Transform2D::operator=(const Transform2D &right) {
+	origin = right.origin;
+	rotation = right.rotation;
+	scale = right.scale;
+}
+
+constexpr void Transform2D::operator*=(const Transform2D &right) {
+	origin += right.origin;
+	rotation += right.rotation;
+	scale *= right.scale;
+}
+
 
 }
