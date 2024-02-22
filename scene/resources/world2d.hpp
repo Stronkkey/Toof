666a9ba441c0cb66df7856d99c82f92bd66da47b
Stronkkey <Ail45000@proton.me>
2024-02-22 17:11:58 +0100

    Implement constexprs in world2d inline

--- scene/resources/world2d.hpp
+++ scene/resources/world2d.hpp
@@ -11,17 +11,13 @@ private:
 	uid space;
 
 public:
-	constexpr void set_space(const uid new_space);
-	constexpr uid get_space() const;
+	constexpr void set_space(const uid new_space) {
+		space = new_space;
+	}
+	constexpr uid get_space() const {
+		return space;
+	}
 };
-
-constexpr void World2D::set_space(const uid new_space) {
-	space = new_space;
-}
-
-constexpr uid World2D::get_space() const {
-	return space;
-}
 
 }
 
