8bac241ec98728e6729c5e4c71145d7eefa5dc8a
Stronkkey <Ail45000@proton.me>
2024-02-24 16:09:54 +0100

    Add lerp functions to math

--- core/math/math_defs.hpp
+++ core/math/math_defs.hpp
@@ -27,33 +27,54 @@ typedef uint_t uid;
 namespace Math {
 
 // An EPSILON constant. Which can be used to compare 2 floating point numbers.
-constexpr const real_t CMP_EPSILON = 0.0000000001;
+constexpr const double CMP_EPSILON = 0.0000000001;
 // A PI constant (3.141592). A ration of circle's circumference to its diameter and amount of radians in half turn.
-constexpr const real_t PI = 3.1415926535897;
+constexpr const double PI = 3.1415926535897932384626433833;
 // A TAU constant (6.283185). An equivalent of PI * 2 and amount of radians in full turn.
-constexpr const real_t TAU = PI * 2.0;
+constexpr const double TAU = PI * 2.0;
 // The ratio between one degree and one radian.
-constexpr const real_t DEGREES_RADIAN_SCALAR = 180.0 / PI;
+constexpr const double DEGREES_RADIAN_SCALAR = 180.0 / PI;
 
 /**
 * @returns the @param rotation_degrees converted to degrees rotation. @see also DEGREES_RADIAN_SCALAR.
 */
-constexpr real_t radians_to_degrees(const real_t rotation_radians) {
+constexpr double radians_to_degrees(const double rotation_radians) {
 	return rotation_radians * DEGREES_RADIAN_SCALAR;
+}
+
+/**
+* @returns the @param rotation_degrees converted to degrees rotation. @see also DEGREES_RADIAN_SCALAR.
+*/
+constexpr float radians_to_degrees(const float rotation_radians) {
+	return rotation_radians * (float)DEGREES_RADIAN_SCALAR;
 }
 
 /**
 * @returns the @param rotation_degrees converted to radians rotation. @see also DEGREES_RADIAN_SCALAR.
 */
-constexpr real_t degrees_to_radians(const real_t rotation_degrees) {
+constexpr double degrees_to_radians(const double rotation_degrees) {
 	return rotation_degrees / DEGREES_RADIAN_SCALAR;
+}
+
+/**
+* @returns the @param rotation_degrees converted to radians rotation. @see also DEGREES_RADIAN_SCALAR.
+*/
+constexpr float degrees_to_radians(const float rotation_degrees) {
+	return rotation_degrees / (float)DEGREES_RADIAN_SCALAR;
 }
 
 /**
 * @returns true if @param left approximately equals @param right; 
 */
-constexpr bool is_equal_approx(const real_t left, const real_t right) {
+constexpr bool is_equal_approx(const double left, const double right) {
 	return left >= (right - CMP_EPSILON) && left <= (right + CMP_EPSILON);
+}
+
+/**
+* @returns true if @param left approximately equals @param right; 
+*/
+constexpr bool is_equal_approx(const float left, const float right) {
+	return left >= (right - (float)CMP_EPSILON) && left <= (right + (float)CMP_EPSILON);
 }
 
 /**
@@ -74,8 +95,15 @@ bool is_equal_approx(const Transform2D &left, const Transform2D &right);
 /**
 * @returns true if @param number is approximately equal to 0. 
 */
-constexpr bool is_zero_approx(const real_t number) {
+constexpr bool is_zero_approx(const double number) {
 	return number >= (0.0 - CMP_EPSILON) && number <= (0.0 + CMP_EPSILON);
+}
+
+/**
+* @returns true if @param number is approximately equal to 0. 
+*/
+constexpr bool is_zero_approx(const float number) {
+	return number >= (0.0 - (float)CMP_EPSILON) && number <= (0.0 + (float)CMP_EPSILON);
 }
 
 /**
@@ -92,6 +120,14 @@ bool is_zero_approx(const Rect2 &rect2);
 * @returns true if the components of @param transform2d approximately equal to 0. 
 */
 bool is_zero_approx(const Transform2D &transform2d);
+
+constexpr double lerp(const double from, const double to, const double weight) {
+	return from + (to - from) * weight;
+}
+
+constexpr float lerp(const float from, const float to, const float weight) {
+	return from + (to - from) * weight;
+}
 
 }
 
