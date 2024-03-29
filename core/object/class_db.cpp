758d2515a505dc498ae3dbc80b5313509dd160c2
Stronkkey <Ail45000@proton.me>
2024-03-29 18:34:15 +0100

    Remove classDB and variant.cpp

--- core/object/class_db.cpp
+++
@@ -1,8 +0,0 @@
-#include <core/object/class_db.hpp>
-
-using namespace sdl;
-
-void ClassDB::add_property(const StringName &class_name, const std::any &property) {
-	(void)class_name;
-	(void)property;
-}
