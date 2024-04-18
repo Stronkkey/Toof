f435246c50cd46c6724f92d239f88d7ec6b8fc86
Stronkkey <Ail45000@proton.me>
2024-04-18 16:37:22 +0200

    Add shell  text modifiers into utilityfunctions

--- core/utility_functions.cpp
+++ core/utility_functions.cpp
@@ -2,3 +2,44 @@
 
 using namespace sdl;
 
+void UtilityFunctions::start_text_modifier(String &string, TextModifier text_modifier) {
+	char _string[5];
+	StringView text_modifier_string = text_modifier_to_string_view(text_modifier);
+	StringView modifier_string;
+
+	_string[0] = '\33';
+	_string[1] = '[';
+
+	if (text_modifier_string.size() == 1) {
+		_string[2] = text_modifier_string[0];
+		_string[3] = 'm';
+		modifier_string = StringView(_string, 4);
+	} else if (text_modifier_string.size() > 1) {
+		_string[2] = text_modifier_string[0];	
+		_string[3] = text_modifier_string[1];
+		_string[4] = 'm';
+		modifier_string  = StringView(_string, 5);
+	}
+	
+	String temp = std::move(string);
+
+	const size_t string_size = modifier_string.length() + temp.length();
+	string.resize(string_size);
+
+	modifier_string.copy(string.data(), modifier_string.length(), 0);
+	memmove(string.data() + modifier_string.length(), temp.data(), temp.length());
+}
+
+void UtilityFunctions::escape_string_text_modifier(String &string) {
+	string.resize(string.length() + 4);
+	string.append("\x1b[0m");
+}
+
+void UtilityFunctions::apply_text_modifier_on_string(String &string, TextModifier text_modifier) {
+	start_text_modifier(string, text_modifier);
+	escape_string_text_modifier(string);
+}
+
+void UtilityFunctions::apply_text_modifiers_on_string(String &string) {
+	start_text_modifier(string, TEXT_MODIFIER_NONE);
+}
--- core/utility_functions.hpp
+++ core/utility_functions.hpp
@@ -81,6 +81,99 @@ inline void prints_err(const Args&... variants) {
 	std::cerr << to_strings(variants...) << '\n';
 }
 
+enum TextModifier {
+	TEXT_MODIFIER_NONE = 0,
+	TEXT_MODIFIER_BOLD = 1,
+	TEXT_MODIFIER_CURSIVE = 2,
+	TEXT_MODIFIER_UNDERLINED = 4,
+	TEXT_MODIFIER_BLINKING = 5,
+	TEXT_MODIFIER_REVERSE_VIDEO = 7,
+	TEXT_MODIFIER_CUT_THROUGH = 9,
+	TEXT_MODIFIER_COLOR_BLACK = 30,
+	TEXT_MODIFIER_COLOR_RED = 31,
+	TEXT_MODIFIER_COLOR_GREEN = 32,
+	TEXT_MODIFIER_COLOR_BROWN = 33,
+	TEXT_MODIFIER_COLOR_BLUE = 34,
+	TEXT_MODIFIER_COLOR_PURPLE = 35,
+	TEXT_MODIFIER_COLOR_CYAN = 36,
+	TEXT_MODIFIER_COLOR_WHITE = 37,
+};
+
+constexpr StringView text_modifier_to_string_view(TextModifier text_modifier) {
+	switch (text_modifier) {
+		case TEXT_MODIFIER_NONE:
+			return "0";
+		case TEXT_MODIFIER_BOLD:
+			return "1";
+		case TEXT_MODIFIER_CURSIVE:
+			return "2";
+		case TEXT_MODIFIER_UNDERLINED:
+			return "4";
+		case TEXT_MODIFIER_BLINKING:
+			return "5";
+		case TEXT_MODIFIER_REVERSE_VIDEO:
+			return "7";
+		case TEXT_MODIFIER_CUT_THROUGH:
+			return "9";
+		case TEXT_MODIFIER_COLOR_BLACK:
+			return "30";
+		case TEXT_MODIFIER_COLOR_RED:
+			return "31";
+		case TEXT_MODIFIER_COLOR_GREEN:
+			return "32";
+		case TEXT_MODIFIER_COLOR_BROWN:
+			return "33";
+		case TEXT_MODIFIER_COLOR_BLUE:
+			return "34";
+		case TEXT_MODIFIER_COLOR_PURPLE:
+			return "35";
+		case TEXT_MODIFIER_COLOR_CYAN:
+			return "36";
+		case TEXT_MODIFIER_COLOR_WHITE:
+			return "37";
+		default:
+			return "0";
+	}
+}
+
+void start_text_modifier(String &string, TextModifier text_modifier);
+void escape_string_text_modifier(String &string);
+void apply_text_modifier_on_string(String &string, TextModifier text_modifier);
+
+void apply_text_modifiers_on_string(String &string);
+
+template<class... Args>
+void apply_text_modifiers_on_string(String &string, Args... text_modifiers) {
+	constexpr const size_t args_count = sizeof...(Args);
+
+	TextModifier text_modifier_array[args_count] = {text_modifiers...};
+	String temp = std::move(string);
+	string = "\x1b[";
+
+	for (size_t i = 0; i < args_count; i++) {
+		StringView text_modifier_string = text_modifier_to_string_view(text_modifier_array[i]);
+
+		bool is_last_element = ((i + 1) == args_count);
+		if (!is_last_element)
+			(string += text_modifier_string) += ';';
+		else
+			string += text_modifier_string;
+	}
+
+	string += 'm';
+
+	String::size_type after_string_size = string.size() + temp.size();
+	String::size_type prev_str_size = string.size();
+	string.resize(string.size() + temp.size() + 4);
+
+	memmove(string.data() + prev_str_size, temp.data(), temp.length());
+
+	string[after_string_size] = '\x1b';
+	string[after_string_size + 1] = '[';
+	string[after_string_size + 2] = '0';
+	string[after_string_size + 3] = 'm';
+}
+
 #define PRINT_LINE(...) ::sdl::UtilityFunctions::print(__VA_ARGS__);
 #define PRINT_LINE_ERR(...) ::sdl::UtilityFunctions::print_err(__VA_ARGS__);
 #define PRINTS_LINE(...) ::sdl::UtilityFunctions::prints(__VA_ARGS__);
