5cab041c2cfb98cca8b01f7eb6a50e6831b420f4
Stronkkey <Ail45000@proton.me>
2024-05-26 21:31:42 +0200

    Add Documentation to Angle

--- core/math/angle.hpp
+++ core/math/angle.hpp
@@ -39,6 +39,12 @@
 
 namespace Toof {
 
+/**
+* @brief A class representing an angle.
+* @details Angle is primarily designed as a substitute for arithmetic types that represent an Angle.
+*
+* There is no direct constructor for Angle, instead, you can use @b Angle::from_degrees or @b Angle::from_radians to construct an Angle.
+*/
 class Angle {
 public:
 	using value_type = real;
@@ -48,126 +54,205 @@ private:
 	explicit constexpr Angle(const value_type angle_degrees): angle_degrees(angle_degrees) {
 	}
 public:
+	/**
+	* @brief Constructs an Angle with no rotation.
+	*/
 	constexpr Angle(): angle_degrees(0.0) {
 	}
 
+	/**
+	* @brief Constructs an Angle from the given Angle.
+	*/
 	constexpr Angle(const Angle &angle): angle_degrees(angle.angle_degrees) {
 	}
 
 	~Angle() = default;
 
+	/**
+	* @brief Sets the rotation of this Angle.
+	* @param angle_degrees the rotation in degrees.
+	*/
 	constexpr void set_angle_degrees(const value_type angle_degrees) {
 		this->angle_degrees = angle_degrees;
 	}
 
+	/**
+	* @brief Sets the rotation of this Angle.
+	* @param angle_radians the rotation in radians.
+	*/
 	constexpr void set_angle_radians(const value_type angle_radians) {
 		this->angle_degrees = Math::radians_to_degrees(angle_radians);
 	}
 
+	/**
+	* @returns The rotation of this Angle in degrees.
+	*/
 	constexpr value_type get_angle_degrees() const {
 		return angle_degrees;
 	}
 
+	/**
+	* @returns The rotation of this Angle in radians.
+	*/
 	constexpr value_type get_angle_radians() const {
 		return Math::degrees_to_radians(angle_degrees);
 	}
 
+	/**
+	* @returns @b true if the rotation of this Angle (in degrees) is exactly equal to 0.
+	*/
 	constexpr bool is_zero_angle() const {
 		return angle_degrees == 0.0;
 	}
 
+	/**
+	* @brief Sets the rotation of this Angle to the Angle given.
+	*/
 	constexpr void operator=(const Angle &right) {
 		angle_degrees = right.angle_degrees;
 	}
 
+	/**
+	* @brief Compares this Angle with the given Angle.
+	* @returns @b true if the rotation of this Angle and the given Angle are exactly equal.
+	*/
 	constexpr bool operator==(const Angle &right) const {
 		return angle_degrees == right.angle_degrees;
 	}
 
+	/**
+	* @brief Compares this Angle with the given Angle.
+	* @returns @b true if the rotation of this Angle and the given Angle are not exactly equal.
+	*/
 	constexpr bool operator!=(const Angle &right) const {
 		return angle_degrees != right.angle_degrees;
 	}
 
+	/**
+	* @brief Adds the rotation of two Angles together.
+	* @details This is equivalent to: @code Angle::from_degrees(left.get_angle_degrees() + right.get_angle_degrees()) @endcode
+	* @returns This Angle's rotation added with the given Angle's rotation.
+	*/
 	constexpr Angle operator+(const Angle &right) const {
 		return Angle(angle_degrees + right.angle_degrees);
 	}
 
+	/**
+	* @brief Unary + does nothing, but sometimes it can make your code more readable.
+	*/
 	constexpr Angle operator+() const {
 		return Angle(+angle_degrees);
 	}
 
+	/**
+	* @brief Adds the rotation of the given Angle to this one.
+	*/
 	constexpr void operator+=(const Angle &right) {
 		angle_degrees += right.angle_degrees;
 	}
 
+	/**
+	* @brief Subtracts the rotation of this Angle from the Angle given.
+	* @details This is equivalent to: @code Angle::from_degrees(left.get_angle_degrees() - right.get_angle_degrees()) @endcode
+	* @returns This Angle's rotation subtracted from the given Angle.
+	*/
 	constexpr Angle operator-(const Angle &right) const {
 		return Angle(angle_degrees - right.angle_degrees);
 	}
 
+	/**
+	* @returns This Angle with the rotation inversed (e.g. a rotation of 56° becomes -56°).
+	*/
 	constexpr Angle operator-() const {
 		return Angle(-angle_degrees);
 	}
 
+	/**
+	* @brief Subtracts this Angle's rotation from the given Angle.
+	*/
 	constexpr void operator-=(const Angle &right) {
 		angle_degrees -= right.angle_degrees;
 	}
 
+	/**
+	* @deprecated This function doesn't have many real use cases. It will be removed in a future version.
+	*/
 	constexpr Angle operator*(const Angle &right) const {
 		return Angle(angle_degrees * right.angle_degrees);
 	}
 
+	/**
+	* @deprecated This function doesn't have many real use cases. It will be removed in a future version.
+	*/
 	constexpr void operator*=(const Angle &right) {
 		angle_degrees *= right.angle_degrees;
 	}
 
+	/**
+	* @deprecated This function doesn't have many real use cases. It will be removed in a future version.
+	*/
 	constexpr Angle operator/(const Angle &right) const {
 		return Angle(angle_degrees / right.angle_degrees);
 	}
 
+	/**
+	* @deprecated This function doesn't have many real use cases. It will be removed in a future version.
+	*/
 	constexpr void operator/=(const Angle &right) {
 		angle_degrees /= right.angle_degrees;
 	}
 	
+	/**
+	* @brief Constructs an Angle with the specified rotation.
+	* @param angle_degrees the rotation in degrees.
+	*/
 	static constexpr Angle from_degrees(const value_type angle_degrees) {
 		return Angle(angle_degrees);
 	}
 
+	/**
+	* @brief Constructs an Angle with the specified rotation.
+	* @param angle_radians the rotation in radians.
+	*/
 	static constexpr Angle from_radians(const value_type angle_radians) {
 		return Angle(Math::radians_to_degrees(angle_radians));
 	}
 
+	/**
+	* @brief Creates a string representation of this Angle.
+	*/
 	[[nodiscard]] operator String() const;
 
 	/**
-	* A constant expression equivalent to an Angle with a rotation of 0 degrees.
+	* @brief A constant expression equivalent to an Angle with no rotation.
 	*/
 	static constexpr Angle ZERO_ROTATION() {
 		return Angle(0.0);
 	}
 
 	/**
-	* A constant expression equivalent to an Angle with a rotation of 90 degrees.
+	* @brief A constant expression equivalent to an Angle with a rotation of 90 degrees.
 	*/
 	static constexpr Angle QUARTER_PAST_ROTATION() {
 		return Angle(90.0);
 	}
 
 	/**
-	* A constant expression equivalent to an Angle with a rotation of 180 degrees.
+	* @brief A constant expression equivalent to an Angle with a rotation of 180 degrees.
 	*/
 	static constexpr Angle HALF_ROTATION() {
 		return Angle(180.0);
 	}
 
 	/**
-	* A constant expression equivalent to an Angle with a rotation of 270 degrees.
+	* @brief A constant expression equivalent to an Angle with a rotation of 270 degrees.
 	*/
 	static constexpr Angle QUARTER_TO_ROTATION() {
 		return Angle(270.0);
 	}
 
 	/**
-	* A constant expression equivalent to an Angle with a rotation of 360 degrees.
+	* @brief A constant expression equivalent to an Angle with a rotation of 360 degrees.
 	*/
 	static constexpr Angle FULL_ROTATION() {
 		return Angle(360.0);
